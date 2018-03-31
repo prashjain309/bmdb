232474c3aacbed4a201d8f47272c27ea54181363
Prashanth Jain <prash.jain309@gmail.com>
2018-03-31 19:11:25 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: './app/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/',
+        publicPath: '',
         filename: 'index-bundle.js'
     },
     devServer: {
