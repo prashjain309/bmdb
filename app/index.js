f721cd7ce4347837d280d98d0e7f93184096b9a7
prashjain309 <prash.jain309@gmail.com>
2018-04-02 00:13:23 +0530

    add redux basics

--- app/index.js
+++ app/index.js
@@ -2,12 +2,16 @@
 var React = require('react');
 var ReactDom = require('react-dom');
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from "react-redux";
+import store from "./store/index";
 import App from './components/app';
 require('./index.less');
 
 ReactDom.render(
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </BrowserRouter>,
     document.getElementById('app')
 );
